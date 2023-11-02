d331f2bd74affbcb0ce135deeabbeaeb2a5091d3
darpbr <dipeixoto@gmail.com>
2023-11-01 21:57:56 -0300

    testes iniciais

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,7 +1,11 @@
 from codigo.bytebank import Funcionario
 
 class TestClass:
+    """Classe de Testes da função principal
+    """
     def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
+        """Teste de idade
+        """
         entrada = '13/03/2000'
         esperado = 23
         
@@ -12,6 +16,8 @@ class TestClass:
         assert resultado == esperado
     
     def test_quando_sobrenome_recebe_lucas_carvalho_deve_retornar_carvahlo(self):
+        """Teste de Sobrenome do funcionário
+        """
         entrada = 'Lucas Carvalho '
         esperado = 'Carvalho'
         
